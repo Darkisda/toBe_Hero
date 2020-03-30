c7f4184a5f69aa9828dac8c89c509a9ae7e0d3f5
Darkisda <luandesouzanascimento2011@gmail.com>
2020-03-29 23:13:26 -0300

    App Finalizado

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
+const { errors } = require('celebrate')
 const cors = require('cors')
 
 const app = express()
@@ -7,9 +8,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 
-app.listen(3333)
+module.exports = app
 /**
  * Rotas: são os links que existe na URL
  * Recursos: são os recursos, como o próprio nome já diz(ksks), que a rota pode oferecer
